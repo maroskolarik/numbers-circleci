273f55a1869bd15e717a188a38e19b9790d6966b
Maroš Kolárik <maroskolarik@mail.com>
2023-07-28 17:08:09 +0200

    Add terraform files

---
+++ tf/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "eu-west-3"
+  access_key = "$AWS_ACCESS"
+  secret_key = "$AWS_SECRET"
+}
+
+resource "aws_security_group" "MK-ODW-2-sg" {
+  name        = "MK-ODW-2-sg"
+  description = "allow 8080 and SSH inbound traffic"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "MK-ODW-2-sg"
+  }
+}
+
+resource "aws_instance" "MK-ODW-2" {
+  ami             = "ami-05b5a865c3579bbc4"
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.MK-ODW-2-sg.name]
+  #key_name        = aws_key_pair.MK-ODW-15-key-pair.key_name
+  user_data       = file("starter-script.sh")
+  tags = {
+    Name = "MK-ODW-2-circleci-deployment"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.MK-ODW-2.public_ip
+}
---
+++ tf/starter-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#allow using password to ssh
+echo -e "password\npassword" | passwd ubuntu
+sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+service ssh restart
+#install docker
+apt update -y
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+apt update -y
+apt install -y docker-ce
+systemctl start docker
+systemctl enable docker
+#run docker container
+docker run -d -p 8080:8080 --restart unless-stopped --name dummy-backend maroskolarik/circleci-app:$CIRCLE_BUILD_NUM
