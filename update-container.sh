78f4f11dd52fbe60e0f33ee592865543e070bf37
Maroš Kolárik <maroskolarik@mail.com>
2023-07-29 15:56:25 +0200

    Update time

--- update-container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sshpass -p $SSH_PASS ssh -o StrictHostKeyChecking=no ubuntu@15.237.113.118
-sudo docker stop dummy-backend || true
-sudo docker rm dummy-backend || true
-sudo docker pull maroskolarik/circleci-app:${CIRCLE_SHA1}
-sudo docker run -d -p 8080:8080 --restart unless-stopped --name dummy-backend maroskolarik/circleci-app:${CIRCLE_SHA1}
