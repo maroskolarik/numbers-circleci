761f4f1378ad687e67650f21c70b1a447461ebe0
Maroš Kolárik <maroskolarik@mail.com>
2023-07-29 09:06:02 +0200

    Update time

--- src/main/java/com/gfa/circleci/controllers/NumberController.java
+++ src/main/java/com/gfa/circleci/controllers/NumberController.java
@@ -21,7 +21,7 @@ public class NumberController {
 
     @GetMapping("/")
     public ResponseEntity index() {
-        return ResponseEntity.status(200).body("CALCULATOR SERVICE 2000 - 0858");
+        return ResponseEntity.status(200).body("CALCULATOR SERVICE 2000 - 0905");
     }
 
     @PostMapping("/")
